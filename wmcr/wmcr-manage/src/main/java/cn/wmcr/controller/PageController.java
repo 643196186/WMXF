423d3b22a309353c4d4e0d46d03f54df02a28947
mac <mac@localhost>
2020-03-23 17:38:03 +0800

    第1.2版:简单新增并启动了manage服务-无错误

---
+++ wmcr/wmcr-manage/src/main/java/cn/wmcr/controller/PageController.java
@@ -0,0 +1,56 @@
+package cn.wmcr.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 在此controller定义页面请求的响应
+ */
+@Controller
+@RequestMapping("/")
+public class PageController {
+	   @RequestMapping("doIndexUI")
+	   public String doIndexUI() {
+		   return "index_wmcr";//返回给DispatcherServlet
+	   }//DispatcherServlet会调用视图解析器对象view进行解析,并将解析结果响应客户端.
+	   /**返回分页页面*/
+	   @RequestMapping("doPageUI")
+	   public String doPageUI() {
+		   //try{Thread.sleep(3000);}catch(Exception e) {}
+		   return "common/page";
+	   }
+	   /** 基于此方法返回日志列表页面*/
+//	   @RequestMapping("log/log_list")
+//	   public String doLogUI() {
+//		   return "sys/log_list";
+//	   }
+	   /** 基于此方法返回菜单列表页面*/
+//	   @RequestMapping("menu/menu_list")
+//	   public String doMenuUI() {
+//		   return "sys/menu_list";
+//	   }
+	   //rest风格(软件架构编码风格)的url
+	   //{}为rest表达式(内容为自己定义的变量)
+	   //@PathVariable 注解用于告诉spring mvc模块,方法参数的值来自url
+	   @RequestMapping("{module}/{moduleUI}")
+	   public String doModuleUI(@PathVariable String moduleUI) {
+		   return "sys/"+moduleUI;
+	   }
+	   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ wmcr/wmcr-zuul/src/main/java/com/wmcr/zuul/Application.java
@@ -0,0 +1,17 @@
+package com.wmcr.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
